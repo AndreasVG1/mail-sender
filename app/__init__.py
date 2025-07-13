763bdaf58aad30e0ef742e2687cdcbe6fd4d1f69
Andreas Villem <andreasvillem@hotmail.com>
2025-07-13 11:56:28 +0300

    db models

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
 
 def create_app() -> Flask:
     app = Flask(__name__)
     
     app.config.from_pyfile("../config.py", silent=True)
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     # Register routes or blueprints
     from .routes import main as main_blueprint
--- app/models.py
+++ app/models.py
@@ -1,12 +1,40 @@
-import sqlalchemy as sa
-from sqlalchemy.orm import sessionmaker, declarative_base
+from app import db
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Text, ForeignKey, DateTime
+from datetime import datetime, timezone
 
-db = sa.create_engine("sqlite://memory:")
-Session = sessionmaker(bind=db)
-base = declarative_base()
+class User(db.Model):
+    __tablename__ = "users"
 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
 
+    templates: Mapped[list["MailTemplate"]] = relationship(back_populates="users")
+    logs: Mapped[list["MailLog"]] = relationship(back_populates="users")
 
-class MailTemplate:
-    def __init__(self, title: str, ):
-        pass
+class MailTemplate(db.Model):
+    __tablename__ = "templates"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    content_html: Mapped[str] = mapped_column(Text, nullable=False)
+    file_path: Mapped[str] = mapped_column(nullable=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="templates")
+    logs: Mapped[list["MailLog"]] = relationship(back_populates="templates")
+
+class MailLog(db.Model):
+    __tablename__ = "logs"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    recipient: Mapped[str] = mapped_column(nullable=False)
+    timestamp: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="logs")
+
+    template_id: Mapped[int] = mapped_column(ForeignKey("templates.id"))
+    template: Mapped["MailTemplate"] = relationship(back_populates="logs")
